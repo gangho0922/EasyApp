6d9808cd680e7a41b574e5311ecdac09cab6f5df
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-01-17 20:09:56 +0900

    Initial Commit

---
+++ MemobySwiftUI/MemobySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemobySwiftUIApp.swift
+//  MemobySwiftUI
+//
+//  Created by AnnKangHo on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct MemobySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
